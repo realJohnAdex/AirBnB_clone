5486c9565b94713d6ba6104ccdba0776913c6f3c
Yinka ☯️ <johnadex60@gmail.com>
2024-03-06 23:07:21 +0000

    Add show and destroy class methods

--- console.py
+++ console.py
@@ -47,6 +47,45 @@ class HBNBCommand(cmd.Cmd):
             print(new_instance.id)
         else:
             pass
+    
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance
+        based on the class name and id.
+        """
+        args = arg.split(' ')
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key in storage.all():
+                print(storage.all()[key])
+            else:
+                print("** no instance found **")
+        
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        (save the change into the JSON file).
+        """
+        args = arg.split(' ')
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key in storage.all():
+                del storage.all()[key]
+                storage.save()
+            else:
+                print("** no instance found **")
 
 
 if __name__ == '__main__':
