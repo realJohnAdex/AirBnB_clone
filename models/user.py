184490bf6cee76f9033570fe871565d41416a3ed
Yinka ☯️ <johnadex60@gmail.com>
2024-03-08 00:03:21 +0000

    Updated user module

--- models/user.py
+++ models/user.py
@@ -7,19 +7,7 @@ class User(BaseModel):
     """
     User class that inherits from BaseModel.
     """
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes User instance with specific attributes.
-        """
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email', "")
-        self.password = kwargs.get('password', "")
-        self.first_name = kwargs.get('first_name', "")
-        self.last_name = kwargs.get('last_name', "")
-
-    def __str__(self):
-        """
-        Returns string representation of User instance.
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
