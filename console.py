b84433300539d0588370fb37c604c9be23207da3
Yinka ☯️ <johnadex60@gmail.com>
2024-03-09 17:30:21 +0000

    Update console.py with improved command interpreter functionality

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-module for console entry point
+module for console entry point of the command interpreter
 """
 import cmd
 # import shlex
@@ -35,13 +35,13 @@ class HBNBCommand(cmd.Cmd):
         """
         super().__init__()
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """
         Exit the program.
         """
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """
         Exit the program.
         """
@@ -53,53 +53,53 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
 
-    def do_create(self, arg):
+    def do_create(self, line):
         """
         Create a new instance of BaseModel,
         save it (to the JSON file) and print the id.
         """
-        if not arg:
+        if not line:
             print("** class name missing **")
-        elif arg not in classes:
+        elif line not in classes:
             print("** class doesn't exist **")
-        elif arg == "BaseModel":
+        elif line == "BaseModel":
             new_instance = BaseModel()
             new_instance.save()
             print(new_instance.id)
-        elif arg == "User":
+        elif line == "User":
             new_instance = User()
             new_instance.save()
             print(new_instance.id)
-        elif arg == "State":
+        elif line == "State":
             new_instance = State()
             new_instance.save()
             print(new_instance.id)
-        elif arg == "City":
+        elif line == "City":
             new_instance = City()
             new_instance.save()
             print(new_instance.id)
-        elif arg == "Amenity":
+        elif line == "Amenity":
             new_instance = Amenity()
             new_instance.save()
             print(new_instance.id)
-        elif arg == "Place":
+        elif line == "Place":
             new_instance = Place()
             new_instance.save()
             print(new_instance.id)
-        elif arg == "Review":
+        elif line == "Review":
             new_instance = Review()
             new_instance.save()
             print(new_instance.id)
         else:
             pass
 
-    def do_show(self, arg):
+    def do_show(self, line):
         """
         Prints the string representation of an instance
         based on the class name and id.
         """
-        args = arg.split(' ')
-        if not arg:
+        args = line.split(' ')
+        if not line:
             print("** class name missing **")
         elif args[0] not in classes:
             print("** class doesn't exist **")
@@ -112,13 +112,13 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** no instance found **")
 
-    def do_destroy(self, arg):
+    def do_destroy(self, line):
         """
         Deletes an instance based on the class name and id
         (save the change into the JSON file).
         """
-        args = arg.split(' ')
-        if not arg:
+        args = line.split(' ')
+        if not line:
             print("** class name missing **")
         elif args[0] not in classes:
             print("** class doesn't exist **")
@@ -132,13 +132,13 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** no instance found **")
 
-    def do_all(self, arg):
+    def do_all(self, line):
         """
         Prints all string representation of all instances
         based or not on the class name.
         """
-        args = arg.split(' ')
-        if not arg:
+        args = line.split(' ')
+        if not line:
             print([str(value) for value in storage.all().values()])
         elif args[0] not in classes:
             print("** class doesn't exist **")
@@ -146,13 +146,13 @@ class HBNBCommand(cmd.Cmd):
             print([str(value) for key, value in storage.all().items()
                    if args[0] in key])
 
-    def do_update(self, arg):
+    def do_update(self, line):
         """
         Updates an instance based on the class name and id
         by adding or updating attribute (save the change into the JSON file).
         """
-        args = arg.split(' ')
-        if not arg:
+        args = line.split(' ')
+        if not line:
             print("** class name missing **")
         elif args[0] not in classes:
             print("** class doesn't exist **")
@@ -169,6 +169,38 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
             else:
                 print("** no instance found **")
+
+    def default(self, line):
+        """
+        Default method for command interpreter.
+        """
+        args = line.split('.')
+        if len(args) >= 2:
+            if args[1] == "all()":
+                self.do_all(args[0])
+            elif args[1] == "count()":
+                count = 0
+                for key, value in storage.all().items():
+                    if args[0] in key:
+                        count += 1
+                print(count)
+            elif args[1][0:4] == "show":
+                new_arg = args[1].split('(')
+                new_arg1 = new_arg[1].split(')')
+                self.do_show(args[0] + " " + new_arg1[0])
+            elif args[1][0:7] == "destroy":
+                new_arg = args[1].split('(')
+                new_arg1 = new_arg[1].split(')')
+                self.do_destroy(args[0] + " " + new_arg1[0])
+            elif args[1][0:6] == "update":
+                new_arg = args[1].split('(')
+                new_arg1 = new_arg[1].split(')')
+                new_arg2 = new_arg1[0].split(', ')
+                self.do_update(args[0] + " " + new_arg2[0] + " " + new_arg2[1] + " " + new_arg2[2])
+            else:
+                print("*** Unknown syntax: {}".format(line))
+        else:
+            print("*** Unknown syntax: {}".format(line))
 
 
 if __name__ == '__main__':
